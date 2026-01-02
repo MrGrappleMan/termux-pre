21a1b06bb8f89c04743cf7feabddac9f37ffa6fb
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-01-02 09:54:13 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@ termux-setup-storage
 pkg update
 pkg upgrade -y
 # Essentials + Languages
-pkg install -y wget curl aria2 proot proot-distro root-repo x11-repo git python hollywood
+pkg install -y wget curl aria2 proot proot-distro root-repo x11-repo git python hollywood fastfetch
 
 # Kali NetHunter( use [1] full image )
 wget -O install-nethunter-termux https://offs.ec/2MceZWr
